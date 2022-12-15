c28723114b4b995226b71822d708fdfeb498f026
BIBIANCHUMSY <bbchumsy@gmail.com>
2022-12-14 16:08:31 -0800

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void print_times_table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
