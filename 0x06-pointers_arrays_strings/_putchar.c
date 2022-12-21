a09a1f60b34d27efab9dc4c59972884c4f2e3b22
BIBIANCHUMSY <bbchumsy@gmail.com>
2022-12-21 11:24:47 -0800

    strcat

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -5,7 +5,7 @@
  * @c: char to be printed
  * Return: 1 on success
  */
-int _putchar(char)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,9 +6,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
 int _putchar(char c);
 
 #endif
