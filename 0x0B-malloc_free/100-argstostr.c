8c51820f0b140e0664784d59cc902d1a69ac7957
BIBIANCHUMSY <bbchumsy@gmail.com>
2023-01-09 05:10:16 -0800

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenate command line arguments.
+ * @ac: count of command line arguments
+ * @av: vector of command line arguments
+ *
+ * Return: string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k;
+	int len, argsLen = 0;
+	char *str, *temp;
+
+	if (!ac || av ==  NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len = 0;
+		temp = av[i];
+
+		while (*temp++ != '\0')
+			len++;
+
+		argsLen += ++len;
+	}
+
+	str = malloc(sizeof(char) * argsLen + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len = 0;
+		temp = av[i];
+
+		while (*temp++ != '\0')
+			len++;
+
+	for (j = 0; j < len; j++, k++)
+		str[k] = av[i][j];
+
+	str[k++] = '\n';
+	}
+
+	str[k] = '\0';
+	return (str);
+}
