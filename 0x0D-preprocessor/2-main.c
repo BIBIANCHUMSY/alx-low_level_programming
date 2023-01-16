61058922f29a72f7e51dc5668b36098c90f6411e
BIBIANCHUMSY <bbchumsy@gmail.com>
2023-01-16 04:16:55 -0800

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
-* main - prints the name of the file
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int ain(void)
 {
 	printf("%s\n", __FILE__);
