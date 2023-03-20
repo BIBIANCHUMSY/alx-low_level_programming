4d9ab3ba992b1f1fd063f39997af1c489b421025
BIBIANCHUMSY <bbchumsy@gmail.com>
2023-03-20 09:44:07 -0700

    update

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -2,32 +2,44 @@
 
 /**
  * _atoi - converts a string to an integer.
- * @s: params
- * Return: something
+ * @s: input string.
+ * Return: integer.
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+	int i, d, n, len, f, digit;
 
-	while (*(s + count) != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-		if (*(s + count) == '-')
-			k *= -1;
+		if (s[i] == '-')
+			++d;
 
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (size > 0)
-				m *= 10;
-			size++;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		count++;
+		i++;
 	}
-	for (i = count - size; i < count; i++)
-	{
-		j = j + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (j * k);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
